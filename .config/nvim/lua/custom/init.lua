fb7104564db6d74626fcf5f3434825710b6e949d
thewildandy <thewildandy@gmail.com>
2023-05-25 23:16:42 +0100

    Add indentation support for typescript

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,1 +1,9 @@
 vim.opt.colorcolumn = "80"
+
+-- set filetype for tsx and jsx files
+-- note the vim-jsx-typescript dots advise setting filetype to typescriptreact
+-- instead of typescript.tsx, but I found that indentation was broken with that
+-- setup. Perhaps in the future it can be implemented as per the documentation.
+vim.api.nvim_exec([[
+  autocmd BufNewFile,BufRead *.tsx,*.jsx set filetype=typescript.tsx
+]], false)
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -2,6 +2,9 @@ local plugins = {
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
-  }
+  },
+  -- fix indentation for tsx files
+  { "leafgarland/typescript-vim" }, -- dependency?
+  { "peitalin/vim-jsx-typescript" }, -- fixes indents
 }
 return plugins
